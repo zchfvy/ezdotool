ef3a7437cd2782b88c7e45d4dcaa597eee86d3fe
Jason Hamilton-Smith <hs.jason@gmail.com>
2017-04-15 23:17:06 -0700

    Add proper package structure
    Setup File
    Command integration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ezdo',
+      version='0.1',
+      description='Easily remember command line tasks',
+      url='https://github.com/zchfvy/ezdotool',
+      author='zchfvy',
+      author_email='zchfvy@zchfvy.com',
+      packages=['ezdo'],
+      scripts=['bin/ezdo']
+      )
